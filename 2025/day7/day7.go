3ae8252d3fc126c4d3879a1baf89fbe6f4d81c3a
adettinger <apddeapp@gmail.com>
2025-12-08 00:42:32 +0100

    day7 part 2

--- 2025/day7/day7.go
+++ 2025/day7/day7.go
@@ -2,6 +2,7 @@ package day7
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/adettinger/adventOfCode-Go/utils"
@@ -10,46 +11,89 @@ import (
 const fileName = "2025/day7/input.txt"
 const sampleFileName = "2025/day7/sampleInput.txt"
 
+type row struct {
+	line   string
+	counts []int
+}
+
+func createRow(line string) row {
+	return row{line, make([]int, len(line))}
+}
+
+func (r row) String() string {
+	var sb strings.Builder
+	for _, i := range r.counts {
+		sb.WriteString(strconv.Itoa(i) + ",")
+	}
+	return fmt.Sprintf("%s %s", r.line, sb.String())
+}
+
 func ProcessFile() {
 	fileToProcess := fileName
 	fmt.Printf("Processing file %q...", fileToProcess)
 	lines := utils.ReadFileToSlice(fileToProcess)
 
 	// replace S with | on first line
-	lines[0] = strings.Replace(lines[0], "S", "|", -1)
-	fmt.Println("Replaced S")
+	// lines[0] = strings.Replace(lines[0], "S", "|", -1)
+
+	rows := convertInputToRows(lines)
+	rows[0] = convertFirstLine(rows[0])
 
 	totalSplits := 0
-	for i := 0; i < len(lines)-1; i++ {
+	for i := 0; i < len(rows)-1; i++ {
 		// fmt.Printf("Processing line %d...\n", i)
 		currSplits := 0
-		lines[i+1], currSplits = processLine(lines[i], lines[i+1])
-		fmt.Printf("%q Found %d splicts\n", lines[i], currSplits)
+		rows[i+1], currSplits = processLine(rows[i], rows[i+1])
+		fmt.Printf("%s Found %d splicts\n", rows[i].String(), currSplits)
 		totalSplits += currSplits
 	}
 
 	fmt.Printf("Found %d splits\n", totalSplits)
+
+	fmt.Printf("Found %d permutations\n", sumInts(rows[len(rows)-1].counts))
+}
+
+func convertFirstLine(input row) row {
+	for i, _ := range input.line {
+		if input.line[i:i+1] == "S" {
+			input.line = replaceCharInString(input.line, "|", i)
+			input.counts[i] = 1
+		}
+	}
+	return input
+}
+
+func convertInputToRows(input []string) []row {
+	result := make([]row, len(input))
+	for index, i := range input {
+		result[index] = createRow(i)
+	}
+	return result
 }
 
 // Assuming there are not 2 ^ in a row
-func processLine(currentLine, nextLine string) (string, int) {
+func processLine(currentRow, nextRow row) (row, int) {
 	// Get position of all beams on current line
-	beams := findBeams(currentLine)
+	beams := findBeams(currentRow.line)
 	countSplits := 0
 	for _, i := range beams {
-		if nextLine[i:i+1] == "^" {
+		currBeamQuant := currentRow.counts[i]
+		if nextRow.line[i:i+1] == "^" {
 			countSplits++
 			if i != 0 {
-				nextLine = replaceCharInString(nextLine, "|", i-1)
+				nextRow.line = replaceCharInString(nextRow.line, "|", i-1)
+				nextRow.counts[i-1] += currBeamQuant
 			}
-			if i != len(nextLine)-1 {
-				nextLine = replaceCharInString(nextLine, "|", i+1)
+			if i != len(nextRow.line)-1 {
+				nextRow.line = replaceCharInString(nextRow.line, "|", i+1)
+				nextRow.counts[i+1] += currBeamQuant
 			}
 		} else {
-			nextLine = replaceCharInString(nextLine, "|", i)
+			nextRow.line = replaceCharInString(nextRow.line, "|", i)
+			nextRow.counts[i] += currBeamQuant
 		}
 	}
-	return nextLine, countSplits
+	return nextRow, countSplits
 }
 
 func replaceCharInString(input, char string, index int) string {
@@ -68,4 +112,12 @@ func findBeams(line string) []int {
 		i += foundIndex + 1
 	}
 	return beams
+}
+
+func sumInts(input []int) int {
+	total := 0
+	for _, i := range input {
+		total += i
+	}
+	return total
 }
